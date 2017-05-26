b8673c56bf7234b0764436d0a8f0d2e51d34ac13
Barbora Jahodníková <Barbora Jahodnkov>
2017-05-26 13:17:50 +0200

    pridane volanie funkcie curl na zistenie volnych tem, par zmien v texte

--- texty/studySK.php
+++ texty/studySK.php
@@ -3,12 +3,12 @@
 Informácie budú dodané neskôr.
 <br>
 
-<p>Kompletný študijný plán pre akademický rok 2017-2018 (<a href="SP20172018b.pdf">SP20172018b.pdf</a>).</p>
+<p>Kompletný študijný plán pre akademický rok 2017-2018 (<a href="./resources/SP20172018b.pdf">SP20172018b.pdf</a>).</p>
 <p>Ďalšie informácie na <a href="http://www.mechatronika.cool">http://www.mechatronika.cool</a></p>
 <br>
 
 <h4>Inžinierske štúdium</h4>
-Prečo študovať na našom ústave?
+<b>Prečo študovať na našom ústave?</b>
 <ul>
     <li>možnosť získať znalosti, ktoré sú implementovateľné v praxi,</li>
     <li>menšie skupiny študentov,</li>
@@ -23,7 +23,7 @@ Prečo študovať na našom ústave?
 <br>
 
 <h5>Študijný program – 1. ročník</h5>
-Zimný semester
+<b>Zimný semester</b>
 <ul>
     <li>CAE mechatronických systémov - tvorba virtuálnych dynamických modelov a ich simulácia</li>
     <li>Metóda konečných prvkov - modelovanie a analýza mechatronických prvkov a systémov</li>
@@ -33,7 +33,7 @@ Zimný semester
 </ul>
 <br>
 
-Letný semester
+<b>Letný semester</b>
 <ul>
     <li>Diplomový projekt 1</li>
     <li>Metódy číslicového riadenia – návrh regulačných obvodov pre modely mechatronických systémov</li>
@@ -43,21 +43,21 @@ Letný semester
 </ul>
 <br>
 
-Možné PVP pre záujemcov o elektroniku
+<b>Možné PVP pre záujemcov o elektroniku</b>
 <ul>
     <li>Inteligentné mechatronické systémy - implementácia metód výpočtovej a umelej inteligencie pre mechatronické systémy</li>
     <li>MEMS - inteligentné senzory a aktuátory - najmodernejšie senzory používané nielen v automobilovom priemysle (akcelerometre, gyroskopy, CCD senzory) a spracovanie signálov vnorenými mikropočítačmi</li>
 </ul>
 <br>
 
-Možné PVP pre záujemcov o automobily
+<b>Možné PVP pre záujemcov o automobily</b>
 <ul>
     <li>Transmisné systémy automobilov a elektromobilov - prevodové mechanizmy automobilov a elektromobilov</li>
     <li>Pohonné systémy a zdroje v elektromobiloch - modelovanie a simulovanie činnosti trakčného a energetického systému elektromobilu</li>
 </ul>
 <br>
 
-Možné PVP pre záujemcov o informatiku
+<b>Možné PVP pre záujemcov o informatiku</b>
 <ul>
     <li>Inteligentné mechatronické systémy - implementácia metód výpočtovej a umelej inteligencie pre mechatronické systémy</li>
     <li>Vybrané kapitoly z automatického riadenia pre mechatroniku - vyrovnávací predmet z automatizácie</li>
@@ -65,7 +65,7 @@ Možné PVP pre záujemcov o informatiku
 </ul>
 <br>
 
-Kompletný študijný plán pre akademický rok 2017-2018 (<a href="SP20172018i.pdf">SP20172018i.pdf</a>).
+Kompletný študijný plán pre akademický rok 2017-2018 (<a href="./resources/SP20172018i.pdf">SP20172018i.pdf</a>).
 <table>
     <tr>
         <td>Prijímacie skúšky na inžinierske štúdium</td><td>28.6.2017 o 10:00 v D124</td>
@@ -88,108 +88,215 @@ Kompletný študijný plán pre akademický rok 2017-2018 (<a href="SP20172018i.
 
 <h3>Bakalárske štúdium</h3>
 <h4>Všeobecné informácie</h4>
-Harmonogram bakalárskeho štúdia
-Zimný semester
-Začiatok výučby v semestri			19. 09. 2016
-Prázdniny					31. 10. 2016
-18. 11. 2016
-23. 12. 2016 – 01. 01. 2017
-Začiatok skúškového obdobia			02. 01. 2017
-Ukončenie skúškového obdobia			12. 02. 2017
-Letný semester
-Začiatok výučby v semestri			13. 02. 2017
-Prázdniny					14. 04. 2017 – 18. 04. 2017
-Začiatok skúškového obdobia			22. 05. 2017
-Ukončenie skúškového obdobia			02. 07. 2017
-Záver bakalárskeho štúdia
-Zadanie záverečnej práce			13. 02. 2017
-Odovzdanie záverečnej práce			19. 05. 2017
-Štátne skúšky bakalárskeho štúdia		06. 07. 2017 – 07. 07. 2017
-Promócie absolventov bakalárskeho štúdia 	14. 09. 2016
-Študijný plán 2016-2017 (SP20162017b.pdf)
-Študijný poriadok (studijny_poriadok.pdf)
-Klasifikačná stupnica (klasifikacna_stupnica.pdf)
-Bakalárske práce
-Pokyny
-Ukončovanie predmetov BP1, BP2, BZP
-Bakalársky projekt 1
-Zodpovedný: 			doc. Ing. Vladimír Kutiš, PhD.
-Hodnotenie predmetu: 		klasifikovaný zápočet
-Štandardný čas plnenia: 	3. roč. bakalárskeho štúdia, zimný semester
-Pre získanie klasifikovaného zápočtu musí študent odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.januára daného roku. Prácu na projekte hodnotí vedúci práce.
-Bakalársky projekt 2
-Zodpovedný: 			doc. Ing. Vladimír Kutiš, PhD.
-Hodnotenie predmetu: 		klasifikovaný zápočet
-Štandardný čas plnenia: 	3. roč. bakalárskeho štúdia, letný semester
-Pre získanie klasifikovaného zápočtu musí študent do dátumu špecifikovanom v harmonograme štúdia FEI STU odovzdať diplomovú prácu:
-1.	v elektronickej forme do AIS
-2.	v tlačenej forme v počte 2 kusy Ing. Sedlárovi? (A803)
-alebo odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.júna daného roku.
-Prácu na projekte hodnotí vedúci práce.
-Bakalárska záverečná práca
-Zodpovedný: 			doc. Ing. Vladimír Kutiš, PhD.
-Hodnotenie predmetu: 		klasifikovaný zápočet
-Štandardný čas plnenia: 	3. roč. bakalárskeho štúdia, letný semester
-Pre získanie skúšky musí študent obhájiť tému svojej diplomovej práce pred štátnicovou komisiou, ktorá zároveň udeľuje známku za obhajobu.
+<h5>Harmonogram bakalárskeho štúdia</h5>
+
+<table>
+    <tr>
+        <td colspan="2"><b>Zimný semester</b></td>
+    </tr><tr>
+        <td>Začiatok výučby v semestri</td><td>19. 09. 2016
+    </tr><tr>
+        <td>Prázdniny</td><td>31. 10. 2016
+    </tr><tr>
+        <td></td><td>18. 11. 2016
+    </tr><tr>
+        <td></td><td>23. 12. 2016 – 01. 01. 2017
+    </tr><tr>
+        <td>Začiatok skúškového obdobia</td><td>02. 01. 2017
+    </tr><tr>
+        <td>Ukončenie skúškového obdobia</td><td>12. 02. 2017
+    </tr><tr>
+        <td colspan="2"><b>Letný semester</b>
+    </tr><tr>
+        <td>Začiatok výučby v semestri</td><td>13. 02. 2017
+    </tr><tr>
+        <td>Prázdniny</td><td>14. 04. 2017 – 18. 04. 2017
+    </tr><tr>
+        <td>Začiatok skúškového obdobia</td><td>22. 05. 2017
+    </tr><tr>
+        <td>Ukončenie skúškového obdobia</td><td>02. 07. 2017
+    </tr><tr>
+        <td colspan="2"><b>Záver bakalárskeho štúdia</b></td>
+    </tr><tr>
+        <td>Zadanie záverečnej práce</td><td>13. 02. 2017
+    </tr><tr>
+        <td>Odovzdanie záverečnej práce</td><td>19. 05. 2017
+    </tr><tr>
+        <td>Štátne skúšky bakalárskeho štúdia</td><td>06. 07. 2017 – 07. 07. 2017
+    </tr><tr>
+        <td>Promócie absolventov bakalárskeho štúdia</td><td>14. 09. 2016
+    </tr>
+</table>
 <br>
-Voľné témy
+
+<p>Študijný plán 2016-2017 (<a href="./resources/SP20162017b.pdf">SP20162017b.pdf</a>)</p>
+
+<p>Študijný poriadok (<a href="./resources/studijny_poriadok.pdf">studijny_poriadok.pdf</a>)</p>
+
+<p>Klasifikačná stupnica (<a href="./resources/klasifikacna_stupnica.pdf">klasifikacna_stupnica.pdf</a>)</p>
 <br>
-<span style="color: red"> Načítať témy z AISu pomocou CURL-u 	(ide o úlohu zo 4. zadania počas semestra).</span>
+
+<h4>Bakalárske práce</h4>
+<h5>Pokyny</h5>
+<b>Ukončovanie predmetov BP1, BP2, BZP</b>
+<table>
+    <tr>
+        <td colspan="2"><b>Bakalársky projekt 1</b>
+    </tr><tr>
+        <td>Zodpovedný:</td><td>doc. Ing. Vladimír Kutiš, PhD.</td>
+    </tr><tr>
+        <td>Hodnotenie predmetu:</td><td>klasifikovaný zápočet</td>
+    </tr><tr>
+        <td>Štandardný čas plnenia:</td><td>3. roč. bakalárskeho štúdia, zimný semester</td>
+    </tr><tr>
+        <td colspan="2"><p>Pre získanie klasifikovaného zápočtu musí študent odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.januára daného roku. Prácu na projekte hodnotí vedúci práce.</p></td>
+    </tr><tr>
+        <td colspan="2"><b>Bakalársky projekt 2</b></td>
+    </tr><tr>
+        <td>Zodpovedný:</td><td>doc. Ing. Vladimír Kutiš, PhD.</td>
+    </tr><tr>
+        <td>Hodnotenie predmetu:</td><td>klasifikovaný zápočet</td>
+    </tr><tr>
+        <td>Štandardný čas plnenia:</td><td>3. roč. bakalárskeho štúdia, letný semester</td>
+    </tr><tr>
+        <td colspan="2">Pre získanie klasifikovaného zápočtu musí študent do dátumu špecifikovanom v harmonograme štúdia FEI STU odovzdať diplomovú prácu:</td>
+    </tr><tr>
+        <td colspan="2"><ul><li>v elektronickej forme do AIS</li></td>
+    </tr><tr>
+        <td colspan="2"><li>v tlačenej forme v počte 2 kusy Ing. Sedlárovi? (A803)</li></td>
+    </tr><tr>
+        <td colspan="2"><li>alebo odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.júna daného roku.</li></ul></td>
+    </tr><tr>
+        <td colspan="2">Prácu na projekte hodnotí vedúci práce.</td>
+    </tr><tr>
+    <td colspan="2"><b>Bakalárska záverečná práca</b></td>
+    </tr><tr>
+        <td>Zodpovedný:</td><td>doc. Ing. Vladimír Kutiš, PhD.</td>
+    </tr><tr>
+        <td>Hodnotenie predmetu:</td><td>klasifikovaný zápočet</td>
+    </tr><tr>
+        <td>Štandardný čas plnenia:</td><td>3. roč. bakalárskeho štúdia, letný semester</td>
+    </tr><tr>
+        <td colspan="2"><p>Pre získanie skúšky musí študent obhájiť tému svojej diplomovej práce pred štátnicovou komisiou, ktorá zároveň udeľuje známku za obhajobu.</p></td>
+    </tr>
+</table>
 <br>
-Inžinierske štúdium
-Všeobecné informácie
-Harmonogram inžinierskeho štúdia
-Zimný semester
-Začiatok výučby v semestri			19. 09. 2016
-Prázdniny					31. 10. 2016
-18. 11. 2016
-23. 12. 2016 – 01. 01. 2017
-Začiatok skúškového obdobia			02. 01. 2017
-Ukončenie skúškového obdobia			12. 02. 2017
-Letný semester
-Začiatok výučby v semestri			13. 02. 2017
-Prázdniny					14. 04. 2017 – 18. 04. 2017
-Začiatok skúškového obdobia			22. 05. 2017
-Ukončenie skúškového obdobia			02. 07. 2017
-Záver inžinierskeho štúdia
-Zadanie diplomovej práce 			13. 02. 2017
-Odovzdanie diplomovej práce 			19. 05. 2017
-Štátne skúšky inžinierskeho štúdia 		13. 06. 2017 – 16. 06. 2017
-Termín promócií 				10. 07. 2017 – 14. 07. 2017
-Študijný plán 2016-2017 (SP20162017i.pdf)
-Študijný poriadok (studijny_poriadok.pdf)
-Klasifikačná stupnica (klasifikacna_stupnica.pdf)
-Diplomové práce
-Pokyny
-Ukončovanie predmetov DP1, DP2, DP3, DZP
-Diplomový projekt 1
-Zodpovedný: 			prof. Ing. Mikuláš Huba, PhD.
-Hodnotenie predmetu: 		klasifikovaný zápočet
-Štandardný čas plnenia: 	1. roč. inžinierskeho štúdia, letný semester
-Pre získanie klasifikovaného zápočtu musí študent odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.júna daného roku. Prácu na projekte hodnotí vedúci práce.
-Diplomový projekt 2
-Zodpovedný: 			prof. Ing. Mikuláš Huba, PhD.
-Hodnotenie predmetu: 		klasifikovaný zápočet
-Štandardný čas plnenia: 	2. roč. inžinierskeho štúdia, zimný semester
-Pre získanie klasifikovaného zápočtu musí študent odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.januára daného roku a obhájiť svoje priebežné výsledky pred minimálne 2-člennou komisiou (jej členom by mal byť vedúci práce). Prácu na projekte hodnotí komisia pri obhajobe, ktorá zoberie do úvahy hodnotenie vedúceho práce.
-Diplomový projekt 3
-Zodpovedný: 			prof. Ing. Mikuláš Huba, PhD.
-Hodnotenie predmetu: 		klasifikovaný zápočet
-Štandardný čas plnenia: 	2. roč. inžinierskeho štúdia, letný semester
-Pre získanie klasifikovaného zápočtu musí študent do dátumu špecifikovanom v harmonograme štúdia FEI STU odovzdať diplomovú prácu:
-1.	v elektronickej forme do AIS
-2.	v tlačenej forme v počte 2 kusy Ing. Sedlárovi? (A803)
-alebo odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.júna daného roku.
-Prácu na projekte hodnotí vedúci práce.
-Diplomová záverečná práca
-Zodpovedný: 			prof. Ing. Mikuláš Huba, PhD.
-Hodnotenie predmetu: 		skúška
-Štandardný čas plnenia: 	2. roč. inžinierskeho štúdia, letný semester
-Pre získanie skúšky musí študent obhájiť tému svojej diplomovej práce pred štátnicovou komisiou, ktorá zároveň udeľuje známku za obhajobu.
+
+<b>Voľné témy</b>
+<?php
+include './curl/curl.php';
+volneTemy('1');
+?>
 <br>
-Voľné témy
+
+<h3>Inžinierske štúdium</h3>
+<h4>Všeobecné informácie</h4>
+<h5>Harmonogram inžinierskeho štúdia</h5>
+<table>
+    <tr>
+        <td><b>Zimný semester</b></td>
+    </tr><tr>
+        <td>Začiatok výučby v semestri</td><td>19. 09. 2016</td>
+    </tr><tr>
+        <td>Prázdniny</td><td>31. 10. 2016</td>
+    </tr><tr>
+        <td></td><td>18. 11. 2016</td>
+    </tr><tr>
+        <td></td><td>23. 12. 2016 – 01. 01. 2017</td>
+    </tr><tr>
+        <td>Začiatok skúškového obdobia</td><td>02. 01. 2017</td>
+    </tr><tr>
+        <td>Ukončenie skúškového obdobia</td><td>12. 02. 2017</td>
+    </tr><tr>
+        <td>Letný semester</td>
+    </tr><tr>
+    <td>Začiatok výučby v semestri</td><td>13. 02. 2017</td>
+    </tr><tr>
+    <td>Prázdniny</td><td>14. 04. 2017 – 18. 04. 2017</td>
+    </tr><tr>
+    <td>Začiatok skúškového obdobia</td><td>22. 05. 2017</td>
+    </tr><tr>
+    <td>Ukončenie skúškového obdobia</td><td>02. 07. 2017</td>
+    </tr><tr>
+    <td>Záver inžinierskeho štúdia</td>
+    </tr><tr>
+    <td>Zadanie diplomovej práce</td><td>13. 02. 2017</td>
+    </tr><tr>
+    <td>Odovzdanie diplomovej práce</td><td>19. 05. 2017</td>
+    </tr><tr>
+    <td>Štátne skúšky inžinierskeho štúdia</td><td>13. 06. 2017 – 16. 06. 2017</td>
+    </tr><tr>
+    <td>Termín promócií</td><td>10. 07. 2017 – 14. 07. 2017</td>
+    </tr>
+</table>
 <br>
-<span style="color: red"> Načítať témy z AISu pomocou CURL-u 	(ide o úlohu zo 4. zadania počas semestra).</span>
+<p>Študijný plán 2016-2017 (<a href="./resources/SP20162017i.pdf">SP20162017i.pdf</a>)</p>
+<p>Študijný poriadok (<a href="./resources/studijny_poriadok.pdf">studijny_poriadok.pdf</a>)</p>
+<p>Klasifikačná stupnica (<a href="./resources/klasifikacna_stupnica.pdf">klasifikacna_stupnica.pdf</a>)</p>
 <br>
-Doktorandské štúdium
-Na stránku stačí dať text “Informácie budú dodané neskôr.”
+
+<h4>Diplomové práce</h4>
+<h5>Pokyny</h5>
+<b>Ukončovanie predmetov DP1, DP2, DP3, DZP</b>
+<table>
+    <tr>
+        <td colspan="2"><b>Diplomový projekt 1</b></td>
+    </tr><tr>
+        <td>Zodpovedný:</td><td>prof. Ing. Mikuláš Huba, PhD.</td>
+    </tr><tr>
+        <td>Hodnotenie predmetu:</td><td>klasifikovaný zápočet</td>
+    </tr><tr>
+        <td>Štandardný čas plnenia:</td><td>1. roč. inžinierskeho štúdia, letný semester</td>
+    </tr><tr>
+        <td colspan="2">Pre získanie klasifikovaného zápočtu musí študent odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.júna daného roku. Prácu na projekte hodnotí vedúci práce.</td>
+    </tr><tr>
+        <td colspan="2"><b>Diplomový projekt 2</b></td>
+    </tr><tr>
+        <td>Zodpovedný:</td><td>prof. Ing. Mikuláš Huba, PhD.</td>
+    </tr><tr>
+        <td>Hodnotenie predmetu:</td><td>klasifikovaný zápočet</td>
+    </tr><tr>
+        <td>Štandardný čas plnenia:</td><td>2. roč. inžinierskeho štúdia, zimný semester</td>
+    </tr><tr>
+        <td colspan="2">Pre získanie klasifikovaného zápočtu musí študent odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.januára daného roku a obhájiť svoje priebežné výsledky pred minimálne 2-člennou komisiou (jej členom by mal byť vedúci práce). Prácu na projekte hodnotí komisia pri obhajobe, ktorá zoberie do úvahy hodnotenie vedúceho práce.</td>
+    </tr><tr>
+        <td colspan="2"><b>Diplomový projekt 3</b></td>
+    </tr><tr>
+        <td>Zodpovedný:</td><td>prof. Ing. Mikuláš Huba, PhD.</td>
+    </tr><tr>
+        <td>Hodnotenie predmetu:</td><td>klasifikovaný zápočet</td>
+    </tr><tr>
+        <td>Štandardný čas plnenia:</td><td>2. roč. inžinierskeho štúdia, letný semester</td>
+    </tr><tr>
+        <td colspan="2">Pre získanie klasifikovaného zápočtu musí študent do dátumu špecifikovanom v harmonograme štúdia FEI STU odovzdať diplomovú prácu:</td>
+    </tr><tr>
+        <td colspan="2">1.	v elektronickej forme do AIS</td>
+    </tr><tr>
+        <td colspan="2">2.	v tlačenej forme v počte 2 kusy Ing. Sedlárovi? (A803)</td>
+    </tr><tr>
+        <td colspan="2">alebo odovzdať technickú dokumentáciu svojmu vedúcemu práce v nim špecifikovanom rozsahu najneskôr do 20.júna daného roku.</td>
+    </tr><tr>
+        <td colspan="2">Prácu na projekte hodnotí vedúci práce.</td>
+    </tr><tr>
+        <td colspan="2"><b>Diplomová záverečná práca</b></td>
+    </tr><tr>
+        <td>Zodpovedný:</td><td>prof. Ing. Mikuláš Huba, PhD.</td>
+    </tr><tr>
+        <td>Hodnotenie predmetu:</td><td>skúška</td>
+    </tr><tr>
+        <td>Štandardný čas plnenia:</td><td>2. roč. inžinierskeho štúdia, letný semester</td>
+    </tr><tr>
+        <td colspan="2">Pre získanie skúšky musí študent obhájiť tému svojej diplomovej práce pred štátnicovou komisiou, ktorá zároveň udeľuje známku za obhajobu.</td>
+    </tr>
+</table>
+<br>
+
+<b>Voľné témy</b>
+<?php
+volneTemy('2');
+?>
+<br>
+
+
+<h3>Doktorandské štúdium</h3>
+<p>Informácie budú dodané neskôr.</p>
