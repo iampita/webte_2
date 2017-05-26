84a0da9b79b00561231fff437e9601a941550a36
Barbora Jahodníková <Barbora Jahodnkov>
2017-05-26 13:23:10 +0200

    'anglicka' verzia stranky study..

---
+++ texty/studyEN.php
@@ -0,0 +1,11 @@
+<h4>Bachelor's degree</h4>
+<p>The information will be delivered later.</p>
+<br>
+
+<h4>Engineering study</h4>
+<p>The information will be delivered later.</p>
+<br>
+
+<h4>Doctoral studies</h4>
+<p>The information will be delivered later.</p>
+<br>
